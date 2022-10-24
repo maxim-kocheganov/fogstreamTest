3f0b17aeeb9489e845c5016c082f125c571f2c5f
max <maxim.k.elasnce@gmail.com>
2022-10-24 18:16:54 +0300

    secret key

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Process:
 ```git clone```
 ```pip install django```
 ```cd map```
-```pip install django```
+goto map/settings.py - change SECRET_KEY
 ```python .\manage.py makemigrations```
 ```python .\manage.py migrate```
 ```python .\manage.py runserver```
--- map/map/settings.py
+++ map/map/settings.py
@@ -18,9 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qw23ov(zn_i0%ktl)k7l7!l#g28wk@f1!hsz3q&@fu=d#pwhz8'
+SECRET_KEY = "asdasdasdasdasdasdas"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
